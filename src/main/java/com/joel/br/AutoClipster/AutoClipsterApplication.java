2a1932f71ec5bede36b2a7cac0474719b06accf6
Joel Santos <joeltere9@gmail.com>
2025-05-27 16:15:09 -0300

    first commit

---
+++ src/main/java/com/joel/br/AutoClipster/AutoClipsterApplication.java
@@ -0,0 +1,13 @@
+package com.joel.br.AutoClipster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoClipsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoClipsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joel/br/AutoClipster/AutoClipsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.br.AutoClipster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoClipsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
